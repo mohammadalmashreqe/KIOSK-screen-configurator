cf9fdd33cc50180f24b9fa87d42329eb6909603d
mohammadalmashreqe <mohammadalmashreqe@yahoo.com>
2019-02-12 15:42:36 +0200

    rest for pray

---
+++ KIOSKScreenConfigurator/Confirmation_activity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIOSKScreenConfigurator
+{
+    class Confirmation_activity:Activity
+    {
+
+
+        int _Timeout;
+
+        public Confirmation_activity (string m , int s ):base (m )
+        {
+            _Timeout = s; 
+        }
+
+        public int Timeout
+        {
+            set
+            {
+                _Timeout = value; 
+               
+            }
+            get
+            {
+                return _Timeout; ; 
+            }
+        }
+    }
+}
---
+++ KIOSKScreenConfigurator/DAL/DataAccessLayer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace KIOSKScreenConfigurator.DAL
+{
+    class DataAccessLayer
+    {
+        SqlConnection sqlConnection; 
+        
+        // constructer to establish the connection 
+        public DataAccessLayer (String conString )
+        {
+            sqlConnection = new SqlConnection(conString);
+        }
+
+
+        // method to open the connection 
+
+        public bool Open ()
+        {
+            if (sqlConnection.State != ConnectionState.Open)
+            {
+                sqlConnection.Open();
+                return true;
+            }
+            else
+                return false; 
+        }
+
+        // method to close the connection 
+        public bool  Close ()
+        {
+            if (sqlConnection.State != ConnectionState.Closed)
+            {
+                sqlConnection.Close();
+                return true;
+            }
+            else
+                return false; 
+        }
+
+        //method to read data from database 
+
+        public DataTable SelectData (string stored_proc , SqlParameter [] param  )
+        {
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+            sqlCommand.CommandText = stored_proc;
+            if(param!=null)
+            {
+                for (int i = 0; i < param.Length; i++)
+                    sqlCommand.Parameters.Add(param[i]);
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt; 
+
+        }
+
+
+        //method to delete insert update to database 
+
+        public 
+    }
+}
